8254b5f63b6e4655073c4dac083d23169398c7c3
Robert Cornell <robert.cornell78@gmai.com>
2016-01-11 16:11:05 -0700

    end of time

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
+  attr_accessor :password
+
   has_secure_password
 
-
-
+  validates :username, presence: true, uniqueness: true
+  validates :password, presence: true, confirmation: true
 end
