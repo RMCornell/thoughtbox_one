6d3b8a18f56d96a1152ea8a46e2f74d87ca45692
Robert Cornell <robert.cornell78@gmai.com>
2016-01-11 11:42:00 -0600

    new users being added to db

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,19 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.create(user_params)
 
+    redirect_to root_path
   end
+
+  def show
+    @user = User.find(id: params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
+
 end
